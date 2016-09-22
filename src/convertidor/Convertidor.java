cb26f747ebf5fb63633fc4c6409c3f57f1ac2887
123Fatima <faty_hdz_00@hotmail.com>
2016-09-22 05:51:04 -0600

    Convertudor
    Fue un examen, el primero

---
+++ src/convertidor/Convertidor.java
@@ -0,0 +1,155 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package convertidor;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+/**
+ *
+ * @author intel
+ */
+public class Convertidor extends JFrame{
+private static final long serialVersionUID = 1583724102189855698L;
+	JTextField pantalla;
+	double resultado;
+	String operacion;
+	JPanel panelNumeros, panelOperaciones;
+	boolean nuevaOperacion = true;
+
+    public Convertidor(){
+        	super();
+		setSize(450, 400);
+		setTitle("Convertidor");
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setResizable(false);
+
+		JPanel panel = (JPanel) this.getContentPane();
+		panel.setLayout(new BorderLayout());
+
+		pantalla = new JTextField("0", 20);
+		pantalla.setBorder(new EmptyBorder(4, 4, 4, 4));
+		pantalla.setFont(new Font("Arial", Font.BOLD, 25));
+		pantalla.setHorizontalAlignment(JTextField.RIGHT);
+		pantalla.setEditable(false);
+		pantalla.setBackground(Color.WHITE);
+		panel.add("North", pantalla);
+
+		panelNumeros = new JPanel();
+		panelNumeros.setLayout(new GridLayout(4, 3));
+		panelNumeros.setBorder(new EmptyBorder(4, 4, 4, 4));
+
+		for (int n = 9; n >= 0; n--) {
+			nuevoBotonNumerico("" + n);
+		}
+
+		nuevoBotonNumerico(".");
+
+		panel.add("Center", panelNumeros);
+
+		panelOperaciones = new JPanel();
+		panelOperaciones.setLayout(new GridLayout(6, 1));
+		panelOperaciones.setBorder(new EmptyBorder(4, 4, 4, 4));
+
+		nuevoBotonOperacion("Convertir");
+		nuevoBotonOperacion("CE");
+		panel.add("East", panelOperaciones);
+
+		validate();
+	}
+
+	/**
+	 * Crea un boton del teclado numérico y enlaza sus eventos con el listener
+	 * correspondiente
+	 * 
+	 * @param digito
+	 *            boton a crear
+	 */
+	private void nuevoBotonNumerico(String digito) {
+		JButton btn = new JButton();
+		btn.setText(digito);
+		btn.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseReleased(MouseEvent evt) {
+				JButton btn = (JButton) evt.getSource();
+				numeroPulsado(btn.getText());
+			}
+		});
+
+		panelNumeros.add(btn);
+	}
+
+	/**
+	 * Crea un botón de operacion y lo enlaza con sus eventos.
+	 * 
+	 * @param operacion
+	 */
+	private void nuevoBotonOperacion(String operacion) {
+		JButton btn = new JButton(operacion);
+		btn.setForeground(Color.RED);
+
+		btn.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseReleased(MouseEvent evt) {
+				JButton btn = (JButton) evt.getSource();
+				operacionPulsado(btn.getText());
+			}
+		});
+
+		panelOperaciones.add(btn);
+	}
+
+	/**
+	 * Gestiona las pulsaciones de teclas numéricas
+	 * 
+	 * @param digito
+	 *            tecla pulsada
+	 */
+	private void numeroPulsado(String digito) {
+		if (pantalla.getText().equals("0") || nuevaOperacion) {
+			pantalla.setText(digito);
+		} else {
+			pantalla.setText(pantalla.getText() + digito);
+		}
+		nuevaOperacion = false;
+	}
+
+	/**
+	 * Gestiona el gestiona las pulsaciones de teclas de operación
+	 * 
+	 * @param tecla
+	 */
+	private void operacionPulsado(String tecla) {
+		if (tecla.equals("Convertir")) {
+			calcularResultado();
+		} else if (tecla.equals("CE")) {
+			resultado = 0;
+			pantalla.setText("");
+			nuevaOperacion = true;
+		} else {
+			operacion = tecla;
+			if ((resultado > 0) && !nuevaOperacion) {
+				calcularResultado();
+			} else {
+				resultado = new Double(pantalla.getText());
+			}
+		}
+
+		nuevaOperacion = true;
+	}
+
+	private void calcularResultado() {
+			resultado = new Double(pantalla.getText()) ;
+                        resultado = resultado * 19;
+		
+		pantalla.setText("" + resultado);
+		operacion = "";
+}
+}
+ 
